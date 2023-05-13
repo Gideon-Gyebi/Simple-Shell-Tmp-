e4b663b843678a70081934fbe27aac23b5304846
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2023-05-13 12:48:38 +0000

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,18 +1,12 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
 
-/* need for read & write buffers */
-#define READ_BUF_SIZE 1024
-#define WRITE_BUF_SIZE 1024
-#define BUF_FLUSH -1
+#define MAX_COMMAND_LENGTH 1024
+
+#endif /* SHELL_H */
