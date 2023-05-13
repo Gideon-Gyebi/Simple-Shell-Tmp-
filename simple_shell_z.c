7a3378493751f061fa59867d644fb2f92b990c5b
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2023-05-13 12:51:38 +0000

    Create simple_shell_z.c

---
+++ simple_shell_z.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 1024
+
+int main() {
+  char command[MAX_COMMAND_LENGTH];
+  char *args[MAX_COMMAND_LENGTH];
+  char *token;
+  int status;
+
+  while (1) {
+    printf("$ ");
+    fflush(stdout);
+
+    // Read command from user input
+    if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
+      break;
+    }
+
+    // Parse command into arguments
+    int i = 0;
+    token = strtok(command, " \n");
+    while (token != NULL) {
+      args[i++] = token;
+      token = strtok(NULL, " \n");
+    }
+    args[i] = NULL;
+
+    // Execute command
+    pid_t pid = fork();
+    if (pid == -1) {
+      perror("fork");
+      exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+      // Child process
+      execvp(args[0], args);
+      perror("execvp");
+      _exit(EXIT_FAILURE);
+    } else {
+      // Parent process
+      waitpid(pid, &status, 0);
+    }
+  }
+
+  return 0;
+}
